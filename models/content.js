79b9e214980609c7fb2b5ecc9731efb605f3df29
Shinhei Hara <s.h.spooky@gmail.com>
2020-01-05 01:47:22 +0900

    fix storeditemid

--- models/content.js
+++ models/content.js
@@ -1,13 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const content = sequelize.define('content', {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    storeditemid: DataTypes.STRING,
   }, {
     underscored: true,
   });
   content.associate = function(models) {
     // associations can be defined here
-    content.hasOne(models.storeditem,{foreignKey:"storeditemid"})
+    content.hasOne(models.storeditem,{foreignKey:"document"})
     content.belongsTo(models.contentgroup,{foreignKey:"group"})
   };
   return content;
